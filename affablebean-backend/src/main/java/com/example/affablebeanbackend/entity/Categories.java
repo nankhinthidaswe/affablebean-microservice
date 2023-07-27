7d74f6fe20f3c3550f28ead75935311b3799b0e6
Nan Khin Thida Swe <nnankhinthidaswe@gmail.com>
2023-07-27 15:06:16 +0630

    Initial commit

---
+++ affablebean-backend/src/main/java/com/example/affablebeanbackend/entity/Categories.java
@@ -0,0 +1,22 @@
+package com.example.affablebeanbackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Data
+@AllArgsConstructor
+public class Categories {
+    private List<Category> categories;
+
+    public Categories(Spliterator<Category> iterable){
+        categories = StreamSupport.stream(iterable, false).collect(Collectors.toList());
+    }
+    public Categories(){}
+}
---
+++ affablebean-backend/src/main/java/com/example/affablebeanbackend/entity/Product.java
@@ -0,0 +1,29 @@
+package com.example.affablebeanbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String  name;
+    private double price;
+    private String description;
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
+    @JoinColumn(name = "category_id")
+    @ManyToOne
+    private Category category;
+
+}
